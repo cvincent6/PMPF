a71b02e2a539c8a76bd0c56622a2e9de5270f920
Gautham Ramesh <Gautham@Gauthams-MacBook-Air.local>
2017-12-05 18:13:58 -0500

    completed WT

--- python/main.py
+++ python/main.py
@@ -12,6 +12,11 @@ import pandas
 import pywt
 from pandas.tools.plotting import autocorrelation_plot
 from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.graphics.tsaplots import plot_pacf
+#from statsmodels.tsa.statespace import SARIMAX
+import statsmodels.api as sm
+import statsmodels.tsa.stattools as st
+import itertools
 
 root = r'dataset/'
 
@@ -22,14 +27,36 @@ training_load= training_data.loc[:,'load']
 
 ''' Wavelet transform'''
 db = pywt.Wavelet('db5')
-[p,l] = [[],[]] # initialize wavelet transform vatiables
+[price,load] = [[],[]] # initialize wavelet transform vatiables
 
-p = pywt.wavedec(training_price,db,level = 3)
-l = pywt.wavedec(training_load,db,level =3)
+price = pywt.wavedec(training_price,db,level = 3)
+load = pywt.wavedec(training_load,db,level =3)
 
 ''' step to estimate p,q,d for arima model?'''
 
 
+#p = d = q = range(0, 4)
+#pdq = list(itertools.product(p, d, q))
+#seasonal_pdq = [(x[0], x[1], x[2], 24) for x in list(itertools.product(p, d, q))]
+#
+##warnings.filterwarnings("ignore") # specify to ignore warning messages
+#
+#for param in pdq:
+#    for param_seasonal in seasonal_pdq:
+#        try:
+#            mod = sm.tsa.statespace.SARIMAX(price[0],
+#                                            order=param,
+#                                            seasonal_order=param_seasonal,
+#                                            enforce_stationarity=False,
+#                                            enforce_invertibility=False)
+#
+#            results = mod.fit()
+#
+#            print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
+##            print results.summary()
+#        except:
+#            continue
+#
 
 
 
